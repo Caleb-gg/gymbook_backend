489e44e1a1fa909fc016a7a6c2c15a2a1d594dbd
Caleb-gg <caleb_gutierrez27@yahoo.com>
2025-11-07 01:08:21 -0800

    first commit

---
+++ src/main/java/com/github/calebgg/gymbook_backend/GymbookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.calebgg.gymbook_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymbookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymbookBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/calebgg/gymbook_backend/GymbookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.calebgg.gymbook_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymbookBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
